51bd14735ecbad2606eece36f161307a83b6ed26
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-13 10:40:21 -0300

    Script para settar a conexão do projeto ao repositório

---
+++ settando_git.R
@@ -0,0 +1,30 @@
+# Pacotes ----
+
+library(usethis)
+
+# Inicial o git ----
+
+usethis::use_git()
+
+# Configure o usuario e email ----
+
+usethis::use_git_config(user.name = "Edbbioeco",
+                        user.email = "edsonbbiologia@gmail.com")
+
+# Settando o repositório ----
+
+usethis::proj_get()
+
+usethis::use_git_remote(name = "origin",
+                        url = "https://github.com/Edbbioeco/Scripts-mestrado.git",
+                        overwrite = TRUE)
+
+
+
+usethis::use_git_remote(name = "origin",
+                        url = "github.com/Edbbioeco?tab=repositories.git",
+                        overwrite = TRUE)
+
+# Renomear o branch do master para main ----
+
+usethis::git_default_branch_rename(from = "master", to = "main")
