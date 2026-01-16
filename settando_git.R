c90692e7a937bfc48a10382ff69fb65af20608cb
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-16 09:32:19 -0300

    Script para settar o repositório Github

--- settando_git.R
+++ settando_git.R
@@ -15,16 +15,20 @@ usethis::use_git_config(user.name = "Edbbioeco",
 
 usethis::proj_get()
 
+## Repositório púbico ----
+
 usethis::use_git_remote(name = "origin",
                         url = "https://github.com/Edbbioeco/Scripts-mestrado.git",
                         overwrite = TRUE)
 
-
+## Repositório privado ----
 
 usethis::use_git_remote(name = "origin",
-                        url = "github.com/Edbbioeco?tab=repositories.git",
+                        url = "https://github.com/Edbbioeco/scripts_tabelas_dados_mestrado.git",
                         overwrite = TRUE)
 
 # Renomear o branch do master para main ----
+
+usethis::git_default_branch_configure()
 
 usethis::git_default_branch_rename(from = "master", to = "main")
