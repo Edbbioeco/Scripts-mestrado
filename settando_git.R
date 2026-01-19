213305a4c6188bcc8dc3bc03843c262fb74eb531
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-19 11:49:36 -0300

    Scripts .R

---
+++ settando_git.R
@@ -0,0 +1,34 @@
+# Pacotes ----
+
+library(usethis)
+
+# Inicial o git ----
+
+usethis::use_git()
+
+# Configure o usuario e email ----
+
+usethis::use_git_config(user.name = "Edbbioeco",
+                        user.email = "edsonbbiologia@gmail.com")
+
+# Settando o repositório ----
+
+usethis::proj_get()
+
+## Repositório púbico ----
+
+usethis::use_git_remote(name = "origin",
+                        url = "https://github.com/Edbbioeco/Scripts-mestrado.git",
+                        overwrite = TRUE)
+
+## Repositório privado ----
+
+usethis::use_git_remote(name = "origin",
+                        url = "https://github.com/Edbbioeco/scripts_tabelas_dados_mestrado.git",
+                        overwrite = TRUE)
+
+# Renomear o branch do master para main ----
+
+usethis::git_default_branch_configure()
+
+usethis::git_default_branch_rename(from = "master", to = "main")
