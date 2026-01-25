815b7a05595b1504903853c4c6172dae0a07c34b
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-25 11:13:20 -0300

    Script para analisar os modelos lineares usando a distância da borda como variável preditora

--- modelos_distancia_borda.R
+++ modelos_distancia_borda.R
@@ -93,7 +93,20 @@ df_beta |> dplyr::glimpse()
 
 ## Criando o modelo ----
 
-## Avaliando o modelo ----
+modelo_alfa <- lm(`Q = 1` ~ `Distância da Borda`,
+                  data = df_alfa)
+
+## Pressupostos do modelo ----
+
+modelo_alfa |> performance::check_model(check = c("homogeneity",
+                                                  "qq",
+                                                  "normality"))
+
+modelo_alfa |> performance::check_heteroscedasticity()
+
+modelo_alfa |> performance::check_normality()
+
+## Estatísticas o modelo ----
 
 ## Dataframe das estatísticas do modelo ----
 
@@ -103,59 +116,12 @@ df_beta |> dplyr::glimpse()
 
 ## Criando o modelo ----
 
-## Avaliando o modelo ----
+## Pressupostos do modelo ----
+
+## Estatísticas o modelo ----
 
 ## Dataframe das estatísticas do modelo ----
 
 ## Gráfico ----
 
 # Ordenação das espécies ----
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-div_alfa2 <- dados_alfa |>
-  vegan::renyi(scales = 0:2, hill = TRUE) |>
-  dplyr::mutate(`Unidade Amostral` =  dados_alfa |> rownames(),
-                Eq = `2` / `1`)
-
-div_alfa2
-
-ambientais2 <- ambientais |>
-  dplyr::left_join(div_alfa2,
-                   by = "Unidade Amostral")
-
-ambientais2 |> as.data.frame()
-
-lm(`1` ~ `Distância da Borda`, data = ambientais2) |>
-  summary()
-
-lm(`1` ~ `Distância da Borda`, data = ambientais2) |>
-  performance::check_model(check = c("homogeneity",
-                                     "qq",
-                                     "normality"))
-
-ambientais2 |>
-  ggplot(aes(`Distância da Borda`, `1`)) +
-  geom_point() +
-  geom_smooth(method = "lm")
-
-dados_alfa
-
-dados_alfa |>
-  dplyr::mutate(`Distância da Borda` = ambientais2$`Distância da Borda`) |>
-  ordenaR::order_circle(gradient = "Distância da Borda",
-                        species = 1:10)
