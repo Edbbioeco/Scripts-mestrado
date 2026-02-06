28b6cfe3f3b4861a53a6d784d95d2695c6145519
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-06 20:16:28 -0300

    Script para analisar o índice de abertura do dossel com o pacote coiR

--- indice_de_dossel.R
+++ indice_de_dossel.R
@@ -13,6 +13,8 @@ diretorios
 
 # Calculando índice de abertura de dossel ----
 
+## Loop ----
+
 indice <- c()
 
 trilha <- c()
@@ -23,7 +25,9 @@ campanha <- c()
 
 calcular_indice <- function(diretorios){
 
-  imagens <- list.files(path = diretorios[5])
+  imagens <- paste0(diretorios,
+                    "/",
+                    list.files(path = diretorios))
 
   indice_calcular <- function(imagens){
 
@@ -34,39 +38,51 @@ calcular_indice <- function(diretorios){
       coiR::coir_binarize(plot = FALSE) |>
       coiR::coir_index(round = 5)
 
+    indice <<- c(indice, indice_dossel)
+
   }
 
-  purrr::map(indice_calcular, imagens)
+  purrr::map(imagens, indice_calcular)
 
-  indice <<- c(indice, indice_dossel)
-
-  trilha_dossel <- dplyr::case_when(diretorios[1] |>
+  trilha_dossel <- dplyr::case_when(diretorios |>
                                       stringr::str_detect("T1") ~ "1",
-                                    diretorios[1] |>
+                                    diretorios |>
                                       stringr::str_detect("T2") ~ "2",
-                                    diretorios[1] |>
+                                    diretorios |>
                                       stringr::str_detect("R") ~ "Ripária")
 
   trilha <- c(trilha, trilha_dossel)
 
-  parcela_dossel <- dplyr::case_when(diretorios[1] |>
+  parcela_dossel <- dplyr::case_when(diretorios |>
                                        stringr::str_detect("P1|R1") ~ "1",
-                                     diretorios[1] |>
+                                     diretorios |>
                                        stringr::str_detect("P2|R2") ~ "2",
-                                     diretorios[1] |>
+                                     diretorios |>
                                        stringr::str_detect("P3") ~ "3",
-                                     diretorios[1] |>
+                                     diretorios |>
                                        stringr::str_detect("P4") ~ "4")
 
   parcela <- c(parcela, parcela_dossel)
 
-  campanha_dossel <- dplyr::case_when(diretorios[1] |>
+  campanha_dossel <- dplyr::case_when(imagens |>
                                         stringr::str_detect("C1") ~ "1",
-                                      diretorios[1] |>
+                                      imagens |>
                                         stringr::str_detect("C2") ~ "2",
-                                      diretorios[1] |>
+                                      imagens |>
                                         stringr::str_detect("C3") ~ "3")
 
   campanha <- c(campanha, campanha_dossel)
 
+  df_dossel <<- tibble::tibble(Trilha = trilha,
+                               Parcela = parcela,
+                               Campanha = campanha,
+                               Índice = indice)
+
 }
+
+purrr::map(.x = diretorios[1],
+           .f = calcular_indice)
+
+## Data frame final ----
+
+df_dossel |> as.data.frame()
