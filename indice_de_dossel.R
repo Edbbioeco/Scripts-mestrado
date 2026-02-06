d86bd59429de28639d9558e529fc0ce9b0df9084
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-06 19:27:25 -0300

    Script para analisar o índice de abertura do dossel com o pacote coiR

---
+++ indice_de_dossel.R
@@ -0,0 +1,14 @@
+# Pacotes ----
+
+library(coiR)
+
+library(writexl)
+
+# lista de diretórios -----
+
+diretorios <- paste0("./imagens de dossel/",
+                     list.files(path = "./imagens de dossel"))
+
+diretorios
+
+
