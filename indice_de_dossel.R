444fb2995086014473c32ce82e0271c89058f7e5
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-06 19:40:41 -0300

    Script para analisar o índice de abertura do dossel com o pacote coiR

--- indice_de_dossel.R
+++ indice_de_dossel.R
@@ -11,4 +11,33 @@ diretorios <- paste0("./imagens de dossel/",
 
 diretorios
 
+# Calculando índice de abertura de dossel ----
 
+indice <- c()
+
+trilha <- c()
+
+parcela <- c()
+
+campanha <- c()
+
+calcular_indice <- function(diretorio){
+
+  imagens <- list.files(path = diretorios[1])
+
+  indice_calcular <- function(imagens){
+
+    raster <- terra::rast(imagens)
+
+    indice_dossel <- raster |>
+      coiR::coir_crop(plot = FALSE) |>
+      coiR::coir_binarize(plot = FALSE) |>
+      coiR::coir_index(round = 5)
+
+  }
+
+  purrr::map(indice_calcular, imagens)
+
+  indice <<- c(indice_dossel, indice)
+
+}
