d1aad2ab9352c0b728009064f25bd60f0db81d29
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-15 17:54:33 -0300

    Script para o gráfico de composição das espécies sob gradientes cap2

--- grafico_composicao_especies_gradiente.R
+++ grafico_composicao_especies_gradiente.R
@@ -96,6 +96,9 @@ design <- c(patchwork::area(1, 1),
     `gg_Elevation` +
     `gg_Hydric stream distance`) +
   patchwork::plot_layout(design = design) +
+  patchwork::plot_annotation(tag_levels = "A") &
+  theme(plot.tag = element_text(face = "bold",
+                                size = 25)) &
   ggview::canvas(height = 10, width = 12)
 
 ggsave("grafico_composicao_especies.png",
