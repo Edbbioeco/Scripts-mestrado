7773665200cb3f328d1cf3a1b92c381497b25684
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-23 21:50:21 -0300

    Script para a tabela de abundância de espécies cap2

--- tabela_especies.R
+++ tabela_especies.R
@@ -6,8 +6,6 @@ library(tidyverse)
 
 library(gt)
 
-library(rmarkdown)
-
 # Dados ----
 
 ## Importando ----
@@ -115,46 +113,3 @@ tabela_gt
 file.remove("tabela_especies.docx")
 
 gt::gtsave(tabela_gt, filename = "tabela_especies.docx")
-
-# Estatísticas sobre as espécies ----
-
-## Quantidade de espécies por família ----
-
-especies |>
-  dplyr::filter(Ordem == "Anura" &
-                  !Epípeto %in% c("natalensis", "mystaceus") &
-                  Gênero != "Frostius" &
-                  Família != "Hylidae") |>
-  dplyr::summarise(Número = Espécie |> dplyr::n_distinct(),
-                   .by = Família) |>
-  dplyr::arrange(Número |> dplyr::desc())
-
-## Abundância máxima por família ----
-
-especies |>
-  dplyr::filter(Ordem == "Anura" &
-                  !Epípeto %in% c("natalensis", "mystaceus") &
-                  Gênero != "Frostius" &
-                  Família != "Hylidae") |>
-  dplyr::summarise(Abundância = Abundância |> sum(),
-                   .by = c(Família, Campanha)) |>
-  dplyr::slice_max(Abundância,
-                   n = 1,
-                   by = Família) |>
-  dplyr::arrange(Abundância |> dplyr::desc())
-
-## Abundância máxima por espécie ----
-
-especies |>
-  dplyr::filter(Ordem == "Anura" &
-                  !Epípeto %in% c("natalensis", "mystaceus") &
-                  Gênero != "Frostius" &
-                  Família != "Hylidae") |>
-  dplyr::summarise(Abundância = Abundância |> sum(),
-                   .by = c(Espécie, Campanha)) |>
-  dplyr::slice_max(Abundância,
-                   n = 1,
-                   by = Espécie) |>
-  dplyr::arrange(Abundância |> dplyr::desc())
-
-
