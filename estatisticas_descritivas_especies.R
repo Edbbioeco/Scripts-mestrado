f8e49e53206fcdc4e0d04a78d884b76829ba9079
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-23 21:45:39 -0300

    Script para as estatísticas descritivas das espécies cap2

---
+++ estatisticas_descritivas_especies.R
@@ -0,0 +1,29 @@
+# Pacotes ----
+
+library(readxl)
+
+library(tidyverse)
+
+library(gt)
+
+library(rmarkdown)
+
+# Dados ----
+
+## Importando ----
+
+especies <- readxl::read_xlsx("levantamento_anuros.xlsx")
+
+## Visualizando ----
+
+especies
+
+especies |> dplyr::glimpse()
+
+## Tratando ----
+
+especies <- especies |>
+  dplyr::mutate(`Segmento da parcelas` = `Segmento da parcelas` |>
+                  as.character())
+
+especies |> dplyr::glimpse()
