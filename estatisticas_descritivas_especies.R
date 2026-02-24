af75f97c115a6c455bb71a6633934a56c2aeebec
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-23 21:49:09 -0300

    Script para as estatísticas descritivas das espécies cap2

--- estatisticas_descritivas_especies.R
+++ estatisticas_descritivas_especies.R
@@ -23,3 +23,46 @@ especies <- especies |>
                   as.character())
 
 especies |> dplyr::glimpse()
+
+
+
+# Estatísticas sobre as espécies ----
+
+## Quantidade de espécies por família ----
+
+especies |>
+  dplyr::filter(Ordem == "Anura" &
+                  !Epípeto %in% c("natalensis", "mystaceus") &
+                  Gênero != "Frostius" &
+                  Família != "Hylidae") |>
+  dplyr::summarise(Número = Espécie |> dplyr::n_distinct(),
+                   .by = Família) |>
+  dplyr::arrange(Número |> dplyr::desc())
+
+## Abundância máxima por família ----
+
+especies |>
+  dplyr::filter(Ordem == "Anura" &
+                  !Epípeto %in% c("natalensis", "mystaceus") &
+                  Gênero != "Frostius" &
+                  Família != "Hylidae") |>
+  dplyr::summarise(Abundância = Abundância |> sum(),
+                   .by = c(Família, Campanha)) |>
+  dplyr::slice_max(Abundância,
+                   n = 1,
+                   by = Família) |>
+  dplyr::arrange(Abundância |> dplyr::desc())
+
+## Abundância máxima por espécie ----
+
+especies |>
+  dplyr::filter(Ordem == "Anura" &
+                  !Epípeto %in% c("natalensis", "mystaceus") &
+                  Gênero != "Frostius" &
+                  Família != "Hylidae") |>
+  dplyr::summarise(Abundância = Abundância |> sum(),
+                   .by = c(Espécie, Campanha)) |>
+  dplyr::slice_max(Abundância,
+                   n = 1,
+                   by = Espécie) |>
+  dplyr::arrange(Abundância |> dplyr::desc())
