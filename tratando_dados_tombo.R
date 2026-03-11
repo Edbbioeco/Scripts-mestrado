158f90fbc41f532e33a7bb4fde70bdcf8d0ef15a
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-11 12:15:39 -0300

    Script para tratamento dos dados do Tombo

--- tratando_dados_tombo.R
+++ tratando_dados_tombo.R
@@ -20,6 +20,10 @@ dados |> dplyr::glimpse()
 
 ## Criando um dataframe com os dados de PIS completos ----
 
+dados_pis <- tibble::tibble(ID = paste("0", 566:603))
 
+dados_pis
+
+dados_pis |> dplyr::glimpse()
 
 
