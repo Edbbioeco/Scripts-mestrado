cc3a41d401b7472d28f7f35cf188eeabda331e79
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-11 12:40:08 -0300

    Script para tratamento dos dados do Tombo

--- tratando_dados_tombo.R
+++ tratando_dados_tombo.R
@@ -16,23 +16,19 @@ dados
 
 dados |> dplyr::glimpse()
 
-## Tratando ----
-
-dados <- dados |>
-  dplyr::mutate(n = dplyr::row_number())
-
-dados
-
-dados |> dplyr::glimpse()
-
 # Tratando ----
 
 ## Criando um dataframe com os dados de PIS completos ----
 
-dados_pis <- tibble::tibble(ID = paste("0", 566:603))
+dados_pis <- tibble::tibble(ID = paste0("0", 566:603))
 
 dados_pis
 
 dados_pis |> dplyr::glimpse()
 
+## Unindo os dataframes ----
 
+dados |>
+  dplyr::right_join(dados_pis,
+                    by = "ID") |>
+  as.data.frame()
