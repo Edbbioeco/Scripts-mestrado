41b0eb35545abc72ab781ba3d1a66b3b95b593b5
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-12 15:07:16 -0300

    Script para identificar a altitude das parcelas

---
+++ identificar_altitude_parcelas.R
@@ -0,0 +1,17 @@
+# Pacotes ----
+
+library(readxl)
+
+library(tidyverse)
+
+library(sf)
+
+# Dados ----
+
+## Anuros -----
+
+### Importando ----
+
+anuros <- readxl::read_xlsx("levantamento_anuros.xlsx")
+
+### Visualizando ----
