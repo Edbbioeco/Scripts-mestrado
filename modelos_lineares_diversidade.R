b406f2290ec58b8f896603a18d30505f1a6adb09
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-19 12:49:25 -0300

    Script para criar modelos lineares de diversidade

--- modelos_lineares_diversidade.R
+++ modelos_lineares_diversidade.R
@@ -269,13 +269,20 @@ q_f
 
 #### Tabela flextable ----
 
-df_q1_estatisticas |>
+df_q1_flex <- df_q1_estatisticas |>
   flextable::flextable() |>
   flextable::align(align = "center", part = "all") |>
   flextable::width(j = 2, width = 2) |>
   flextable::fontsize(size = 12, part = "all") |>
   flextable::bg(part = "all", bg = "white") |>
   flextable::bg(i = ~abs(t) > q_t & `F` >= q_f, bg = "gray")
+
+df_q1_flex
+
+## Exportando a tabela ----
+
+sts_df_flex |>
+  flextable::save_as_docx(path = "tabela_estatisticas_modelos_ocupacao.docx")
 
 ### Gráfico -----
 
