d724989e38546a768d6dba5febbfa863c544781b
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-21 11:01:57 -0300

    Script para montar a prancha das espécies

--- prancha_fotos_especies.R
+++ prancha_fotos_especies.R
@@ -10,4 +10,23 @@ library(patchwork)
 
 # Fotos ----
 
+## Lista de fotos ----
+
+foto <- list.files(pattern = "^foto_")
+
+foto
+
 ## Importando ----
+
+importar_fotos <- function(foto){
+
+  foto_importada <- terra::rast(foto)
+
+  nome_sps <- foto |>
+    stringr::str_replace("_", " ") |>
+    stringr::str_extract("\\w+$")
+
+  assign(paste0("foto_", nome_sps),
+         nome_sps)
+
+}
