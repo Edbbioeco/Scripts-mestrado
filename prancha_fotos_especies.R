485aadbc0506791c730eec7a31fb817fa04c7c07
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-21 11:04:05 -0300

    Script para montar a prancha das espécies

--- prancha_fotos_especies.R
+++ prancha_fotos_especies.R
@@ -24,9 +24,13 @@ importar_fotos <- function(foto){
 
   nome_sps <- foto |>
     stringr::str_replace("_", " ") |>
+    stringr::str_remove(".jpg|.jpeg|.JPG") |>
     stringr::str_extract("\\w+$")
 
   assign(paste0("foto_", nome_sps),
-         nome_sps)
+         foto_importada,
+         envir = globalenv())
 
 }
+
+purrr::map(foto, importar_fotos)
