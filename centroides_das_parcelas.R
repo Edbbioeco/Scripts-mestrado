e5392113e635432888991c09cd92ca453d9cc325
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-30 16:09:53 -0300

    Alterado o nome das parcelas

--- centroides_das_parcelas.R
+++ centroides_das_parcelas.R
@@ -19,3 +19,16 @@ parcelas
 ggplot() +
   geom_sf(data = parcelas, color = "black", linewidth = 1)
 
+## Tratando ----
+
+parcelas_trat <- parcelas |>
+  dplyr::rename("Parcela" = Trlh.Pr) |>
+  dplyr::mutate(Parcela = paste0(c(paste0("T", rep(c(1, 2, 3),
+                                                   times = c(4, 4, 2))),
+                                   paste0("R", 1:2)),
+                                 c(paste0("P", rep(1:4,
+                                                   times = 2)),
+                                   paste0("P", 1:2),
+                                   paste0("R", 1:2))))
+
+parcelas_trat
