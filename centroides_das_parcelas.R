110f94526fa3ce3e05e0bb4210c9578f920bb69e
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-30 15:52:37 -0300

    Importados e visualizados os dados

--- centroides_das_parcelas.R
+++ centroides_das_parcelas.R
@@ -5,3 +5,17 @@ library(sf)
 library(tidyverse)
 
 library(writexl)
+
+# Dados ----
+
+## Importando ----
+
+parcelas <- sf::st_read("saltinho_ppbio_parcelas.shp")
+
+## Visualizando ----
+
+parcelas
+
+ggplot() +
+  geom_sf(data = parcelas, color = "black", linewidth = 1)
+
