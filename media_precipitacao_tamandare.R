fac3f9692df90c958a44945304c84ab37aa6105d
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-06 13:13:48 -0300

    Tratamento dos dados para agrupar as colunas dos meses no formato longger

--- media_precipitacao_tamandare.R
+++ media_precipitacao_tamandare.R
@@ -11,3 +11,20 @@ library(writexl)
 ## Importando ----
 
 prec <- readxl::read_xlsx("dados_apac_pluv.xlsx")
+
+## Visualizando ----
+
+prec
+
+prec |> dplyr::glimpse()
+
+# Média de precipitação ----
+
+## Tratando ----
+
+prec_trat <- prec |>
+  tidyr::pivot_longer(cols = Janeiro:Dezembro,
+                      names_to = "Mês",
+                      values_to = "Precipitação")
+
+prec_trat
