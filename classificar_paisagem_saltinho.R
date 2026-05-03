eb0aa4ce32068a35ffce369dbecd86a58866a4cf
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-03 19:26:38 -0300

    Importar e visualizar a imagem de satélite de Saltinho

--- classificar_paisagem_saltinho.R
+++ classificar_paisagem_saltinho.R
@@ -36,3 +36,18 @@ saltinho
 
 ggplot() +
   geom_sf(data = saltinho)
+
+## Imagem de satélite ----
+
+### Baixar ----
+
+saltinho_sat <- terra::rast("saltinho.tif")
+
+### Visualizar ----
+
+saltinho_sat
+
+ggplot() +
+  tidyterra::geom_spatraster_rgb(data = saltinho_sat) +
+  geom_sf(data = saltinho, color = "red", fill = "transparent", size = 1) +
+  coord_sf(expand = FALSE)
