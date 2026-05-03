6d0d581515c5649841018865a9bf223b5dfabc16
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-03 19:12:54 -0300

    Lista de pacotes do script

---
+++ classificar_paisagem_saltinho.R
@@ -0,0 +1,23 @@
+# Pacotes ----
+
+library(sf)
+
+library(tidyverse)
+
+library(leaflet)
+
+library(leaflet.extras)
+
+library(leafem)
+
+library(maptiles)
+
+library(tidyterra)
+
+library(terra)
+
+library(randomForest)
+
+library(ggspatial)
+
+library(ggview)
