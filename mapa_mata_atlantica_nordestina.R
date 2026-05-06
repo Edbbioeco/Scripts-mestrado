1bc2a56923582c505656fb3286026405fff495e3
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-06 13:55:25 -0300

    Filtrar e visualizar o shapefile dos estados do Nordeste

--- mapa_mata_atlantica_nordestina.R
+++ mapa_mata_atlantica_nordestina.R
@@ -18,3 +18,18 @@ br
 
 ggplot() +
   geom_sf(data = br, color = "black")
+
+## Shapefile dos estados do Nordeste ----
+
+### Filtrar ----
+
+ne <- br |>
+  dplyr::filter(cod_rgn == 2)
+
+### Visualizar ----
+
+ne
+
+ggplot() +
+  geom_sf(data = br, color = "black") +
+  geom_sf(data = ne, color = "black", fill = "goldenrod")
