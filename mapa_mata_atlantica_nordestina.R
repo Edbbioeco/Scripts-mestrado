eacb64581813292b3873519eab01429d9568e54b
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-06 14:15:44 -0300

    Importar, recortar e visualizar o shapefile das unidades de conservação da Mata AtlÂntica Nordestina

--- mapa_mata_atlantica_nordestina.R
+++ mapa_mata_atlantica_nordestina.R
@@ -49,3 +49,22 @@ ggplot() +
   geom_sf(data = br, color = "black") +
   geom_sf(data = ne, color = "black", fill = "goldenrod") +
   geom_sf(data = ma, color = "darkgreen", fill = "transparent")
+
+## Sahepfile das unidades de conservaão da Mata Altântica Nordestina ----
+
+### Importar e recortar ----
+
+uc <- sf::st_read("C:/Users/LENOVO/OneDrive/Documentos/aula_geoespacial/unidade_conservacao.shp") |>
+  sf::st_intersection(ma |>
+                        sf::st_intersection(ne |>
+                                              sf::st_union()))
+
+### Visulizar ----
+
+uc
+
+ggplot() +
+  geom_sf(data = br, color = "black") +
+  geom_sf(data = ne, color = "black", fill = "goldenrod") +
+  geom_sf(data = ma, color = "darkgreen", fill = "transparent") +
+  geom_sf(data = uc, color = "darkred", fill = "darkred", alpha = 0.3)
