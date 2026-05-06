d587a551689aa06c63b53ebb645006c142f49c73
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-06 13:49:06 -0300

    Importar e visualizar o shapefile dos estados do Brasil

--- mapa_mata_atlantica_nordestina.R
+++ mapa_mata_atlantica_nordestina.R
@@ -5,3 +5,18 @@ library(geobr)
 library(tidyverse)
 
 library(sf)
+
+# Dados ----
+
+## Shapefile dos Estados do Brasil ----
+
+### Importar ----
+
+br <- sf::st_read("C:/Users/LENOVO/OneDrive/Documentos/aula_geoespacial/br.shp")
+
+### Visualizar ----
+
+br
+
+ggplot() +
+  geom_sf(data = br, color = "black")
