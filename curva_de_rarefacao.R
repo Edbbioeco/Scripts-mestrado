87ea7256d6464c0878b02b6583a5efd8c1b6d1f7
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-08 16:17:37 -0300

    Calcular o estimador de Chao1

--- curva_de_rarefacao.R
+++ curva_de_rarefacao.R
@@ -36,3 +36,28 @@ comp_trat <- comp |>
                      values_fill = 0)
 
 comp_trat
+
+# Curva de rarefação ----
+
+## Calcular curva de rarefação ----
+
+chao_df <- comp_trat |>
+  dplyr::select(dplyr::where(is.numeric)) |>
+  vegan::estaccumR(permutations = 1000) |>
+  summary(display = c("S", "chao")) %>%
+  .[[1]] |>
+  as.data.frame() |>
+  dplyr::rename("Sampling record" = N,
+                "Richness" = S) |>
+  dplyr::mutate(Tipo = "Observed") |>
+  dplyr::bind_rows(comp_trat |>
+                     dplyr::select(dplyr::where(is.numeric)) |>
+                     vegan::estaccumR(permutations = 1000) |>
+                     summary(display = c("S", "chao")) %>%
+                     .[[2]] |>
+                     as.data.frame() |>
+                     dplyr::rename("Sampling record" = N,
+                                   "Richness" = Chao) |>
+                     dplyr::mutate(Tipo = "Estimated"))
+
+chao_df
