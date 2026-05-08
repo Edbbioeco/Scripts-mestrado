4c9333cde8242db7c1b3006a6527fc5aa6c2b2a7
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-08 15:49:41 -0300

    Importar e visualizar os dados de abundância das espécies

--- curva_de_rarefacao.R
+++ curva_de_rarefacao.R
@@ -7,3 +7,15 @@ library(tidyverse)
 library(vegan)
 
 library(ggview)
+
+# Dados ----
+
+## Importar ----
+
+comp <- readxl::read_xlsx("levantamento_anuros.xlsx")
+
+## Visualizar ----
+
+comp
+
+comp |> dplyr::glimpse()
