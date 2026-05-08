6f42b6df7b87532d34b7a66c17bc53dde6c6f878
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-08 16:33:25 -0300

    Gráfico da curva de rarefação

--- curva_de_rarefacao.R
+++ curva_de_rarefacao.R
@@ -43,7 +43,7 @@ comp_trat
 
 chao_df <- comp_trat |>
   dplyr::select(dplyr::where(is.numeric)) |>
-  vegan::estaccumR(permutations = 1000) |>
+  vegan::estaccumR() |>
   summary(display = c("S", "chao")) %>%
   .[[1]] |>
   as.data.frame() |>
@@ -52,7 +52,7 @@ chao_df <- comp_trat |>
   dplyr::mutate(Tipo = "Observed") |>
   dplyr::bind_rows(comp_trat |>
                      dplyr::select(dplyr::where(is.numeric)) |>
-                     vegan::estaccumR(permutations = 1000) |>
+                     vegan::estaccumR() |>
                      summary(display = c("S", "chao")) %>%
                      .[[2]] |>
                      as.data.frame() |>
@@ -61,3 +61,36 @@ chao_df <- comp_trat |>
                      dplyr::mutate(Tipo = "Estimated"))
 
 chao_df
+
+## Gráfico ----
+
+chao_df |>
+  ggplot(aes(`Sampling record`, Richness, color = Tipo, fill = Tipo)) +
+  geom_ribbon(aes(ymax = Richness + Std.Dev,
+                  ymin = Richness - Std.Dev),
+              alpha = 0.3,
+              linewidth = 0) +
+  geom_line(linewidth = 1) +
+  geom_point(shape = 21, size = 5, color = "black", stroke = 1) +
+  labs(fill = NULL,
+       color = NULL) +
+  scale_x_continuous(breaks = seq(0, 32, 2)) +
+  scale_y_continuous(breaks = seq(0, 14, 1),
+                     limits = c(0, 14)) +
+  scale_color_manual(values = c("royalblue", "orange")) +
+  scale_fill_manual(values = c("royalblue", "orange")) +
+  theme_bw() +
+  theme(axis.text = element_text(color = "black", size = 17.5),
+        axis.title = element_text(color = "black", size = 17.5),
+        legend.text = element_text(color = "black", size = 17.5),
+        panel.border = element_rect(color = "black", linewidth = 1),
+        strip.text = element_text(color = "black", size = 20),
+        legend.position = "bottom",
+        title = element_text(color = "black", size = 17.5),
+        panel.background = element_rect(color = "black", linewidth = 1)) +
+  ggview::canvas(height = 10,
+                 width = 12)
+
+ggsave(filename = "curva_de_rarefacao.png",
+       height = 10,
+       width = 12)
