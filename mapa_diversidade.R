47607bd99c6659c829e0f74da91b7e08c331eb07
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-08 12:49:19 -0300

    Mapa de riqueza

--- mapa_diversidade.R
+++ mapa_diversidade.R
@@ -92,3 +92,52 @@ parcelas_div <- parcelas |>
   dplyr::bind_cols(div_tax)
 
 parcelas_div
+
+# Mapa ----
+
+## Mapa de riqueza ----
+
+ggplot() +
+  tidyterra::geom_spatraster(data = alt) +
+  geom_sf(data = borda,
+          aes(color = "Native vegetation"),
+          linewidth = 1,
+          fill = "transparent") +
+  geom_sf(data = parcelas_div,
+          aes(size = Richness),
+          fill = "forestgreen",
+          color = "black",
+          shape = 21,
+          stroke = 1) +
+  tidyterra::scale_fill_whitebox_c(palette = "arid",
+                                   direction = -1,
+                                   name = "Altitude",
+                                   guide = guide_colorbar(order = 1,
+                                                          title.position = "top",
+                                                          title.hjust = 0.5,
+                                                          barwidth = 20,
+                                                          frame.colour = "black",
+                                                          frame.linewidth = 1,
+                                                          ticks.colour = "black",
+                                                          ticks.linewidth = 1)) +
+  scale_color_manual(values = "darkgreen",
+                     guide = guide_legend(order = 2)) +
+  scale_size_continuous(breaks = seq(4, 6, 1),
+                        guide = guide_legend(order = 3,
+                                             title.position = "top",
+                                             title.hjust = 0.5,)) +
+  scale_x_continuous(expand = FALSE,
+                     breaks = seq(-35.19509, -35.15463, 0.03)) +
+  scale_y_continuous(expand = FALSE) +
+  labs(x = NULL,
+       y = NULL,
+       colour = NULL) +
+  coord_sf(label_graticule = "NSWE") +
+  theme_bw() +
+  theme(axis.text = element_text(color = "black", size = 17.5),
+        legend.text = element_text(color = "black", size = 17.5),
+        legend.title = element_text(color = "black", size = 17.5),
+        strip.background = element_rect(color = "black", linewidth = 1),
+        legend.position = "bottom",
+        legend.background = element_blank()) +
+  ggview::canvas(height = 10, width = 12)
