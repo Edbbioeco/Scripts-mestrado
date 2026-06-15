ed445dcf590ff395c037bb2f43354505a8d65b04
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-15 19:30:24 -0300

    Remover código extra, redundante e lento

--- prancha_fotos_especies.R
+++ prancha_fotos_especies.R
@@ -18,22 +18,6 @@ foto
 
 ## Importando ----
 
-importar_fotos <- function(foto){
-
-  foto_importada <- terra::rast(foto) |>
-    terra::flip(direction = "vertical")
-
-  nome_sps <- foto |>
-    stringr::str_replace_all("_", " ") |>
-    stringr::str_remove(".jpg|.jpeg|.JPG") |>
-    stringr::str_extract("\\w+$")
-
-  assign(paste0("foto_", nome_sps),
-         foto_importada,
-         envir = globalenv())
-
-}
-
 fotos_unidas <- purrr::map(foto,
            purrr::in_parallel(~terra::rast(.x) |>
                                 terra::flip(direction = "vertical")),
@@ -45,15 +29,7 @@ fotos_unidas <- purrr::map(foto,
 
 ## Visualizando ----
 
-### Lista das fotos ----
-
-fotos_unidas <- ls(pattern = "^foto_") |>
-  rev() |>
-  mget(envir = globalenv())
-
 fotos_unidas
-
-### Gráfico ----
 
 visualizar_fotos <- function(fotos_unidas){
 
