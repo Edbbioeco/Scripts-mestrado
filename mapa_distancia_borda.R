edc593d0136aabe146ffdd9563f393a7ebc0cae5
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-17 18:38:41 -0300

    Criar ggplot

--- mapa_distancia_borda.R
+++ mapa_distancia_borda.R
@@ -51,3 +51,24 @@ ggplot() +
   geom_sf(data = borda) +
   geom_sf(data = dist_borda, color = "red") +
   geom_sf(data = parcelas, color = "blue")
+
+## Gráfico ----
+
+ggplot() +
+  geom_sf(data = borda, color = "green4", fill = "transparent",
+          linewidth = 1) +
+  geom_sf(data = dist_borda, color = "red", linewidth = 1) +
+  geom_sf(data = parcelas |>
+            dplyr::filter(Trlh.Pr != "1-1"),
+          color = "blue", linewidth = 1) +
+  coord_sf(label_graticule = "NSWE") +
+  ggspatial::annotation_scale(location = "tr",
+                              text_face = "bold",
+                              text_cex = 2,
+                              text_col = "black",
+                              unit_category = "metric",
+                              bar_cols = c("black", "gold")) +
+  theme_bw() +
+  theme(axis.text = element_text(color = "black", size = 20),
+        panel.border = element_rect(color = "black", size = 1)) +
+  ggview::canvas(height = 10, width = 12)
