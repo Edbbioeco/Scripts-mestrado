e8a64458dd7be0966ad23527eb9420cab160c176
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-17 18:17:33 -0300

    Importar e visualizar o shapefile da borda

--- mapa_distancia_borda.R
+++ mapa_distancia_borda.R
@@ -20,3 +20,17 @@ parcelas
 
 ggplot() +
   geom_sf(data = parcelas)
+
+## Borda de Saltinho ----
+
+### Importar ----
+
+borda <- sf::st_read("borda_saltinho.shp")
+
+### Visualizar ----
+
+borda
+
+ggplot() +
+  geom_sf(data = borda) +
+  geom_sf(data = parcelas)
