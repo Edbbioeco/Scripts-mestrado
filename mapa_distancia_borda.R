41ea7bd4b4744afb6e01c1f57e5b0d27eed47cf1
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-17 18:30:22 -0300

    Shapefile das menores distâncias

--- mapa_distancia_borda.R
+++ mapa_distancia_borda.R
@@ -34,3 +34,20 @@ borda
 ggplot() +
   geom_sf(data = borda) +
   geom_sf(data = parcelas)
+
+# Distância da borda ----
+
+## Shapefile da distância mínima de cada parcela ----
+
+dist_borda <- parcelas |>
+  dplyr::filter(Trlh.Pr != "1-1") |>
+  sf::st_centroid() |>
+  sf::st_nearest_points(borda |>
+                          sf::st_boundary())
+
+dist_borda
+
+ggplot() +
+  geom_sf(data = borda) +
+  geom_sf(data = dist_borda, color = "red") +
+  geom_sf(data = parcelas, color = "blue")
