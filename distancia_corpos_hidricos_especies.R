2519ffe60946201557416933e64a9af6be296052
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-18 10:26:39 -0300

    Calcular os shapefiles de distância

--- distancia_corpos_hidricos_especies.R
+++ distancia_corpos_hidricos_especies.R
@@ -206,6 +206,36 @@ delt_dist
 
 ### Distância vertical ----
 
+#### Shapefile das menores distâncias entre as parcelas e os corpos hídricos ----
+
+corpos_hid_ref <- c(hidrico |>
+                      dplyr::filter(geom |>
+                                      sf::st_geometry_type() |>
+                                      stringr::str_detect("POLYGON")) |>
+                      sf::st_geometry() |>
+                      sf::st_boundary(),
+                    hidrico |>
+                      dplyr::filter(geom |>
+                                      sf::st_geometry_type() |>
+                                      stringr::str_detect("LINESTRING")) |>
+                      sf::st_geometry()) |>
+  sf::st_union()
+
+corpos_hid_ref
+
+dist_hid <- parcelas |>
+  sf::st_nearest_points(corpos_hid_ref) |>
+  sf::st_as_sf() |>
+  dplyr::mutate(Trlh.Pr = parcelas$Trlh.Pr)
+
+dist_hid
+
+ggplot() +
+  geom_sf(data = borda) +
+  geom_sf(data = corpos_hid, color = "blue") +
+  geom_sf(data = dist_hid, color = "red") +
+  geom_sf(data = parcelas, color = "black")
+
 #### Coordenadas das menores distâncias ----
 
 corpo_proximo_acude <- sf::st_nearest_feature(parcelas |>
