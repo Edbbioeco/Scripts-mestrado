597189dccf977b8aba1d84ba56a970833141c5a5
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-21 13:27:12 -0300

    Primeiro rascunho do mapa

--- mapa_distribuição_especies.R
+++ mapa_distribuição_especies.R
@@ -98,3 +98,55 @@ parcela_abund <- parcelas[-1, ] |>
                       values_to = "Abundância")
 
 parcela_abund
+
+## Confecionar mapa ----
+
+ggplot() +
+  tidyterra::geom_spatraster(data = alt) +
+  tidyterra::scale_fill_whitebox_c(palette = "arid",
+                                   direction = -1,
+                                   name = "Altitude",
+                                   guide = guide_colorbar(order = 1,
+                                                          title.position = "top",
+                                                          title.hjust = 0.5,
+                                                          barwidth = 15,
+                                                          frame.colour = "black",
+                                                          frame.linewidth = 1,
+                                                          ticks.colour = "black",
+                                                          ticks.linewidth = 1)) +
+  scale_x_continuous(expand = c(0, 0),
+                     breaks = seq(-35.19509, -35.15463, 0.03)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  ggnewscale::new_scale_fill() +
+  geom_sf(data = hid, aes(color = "Water streams"),
+          fill = "blue",
+          linewidth = 1) +
+  geom_sf(data = borda, aes(color = "Forest environment"),
+          linewidth = 1, fill = "transparent") +
+  geom_sf(data = parcela_abund, aes(size = Abundância),
+          color = "black",
+          linewidth = 1) +
+  scale_color_manual(values = c("Forest environment" = "darkgreen",
+                                "Water streams" = "blue")) +
+  scale_size_continuous(breaks = seq(0, 35, 5),
+                        guide = guide_legend(order = 2,
+                                             title = "Abundance",
+                                             title.position = "top",
+                                             title.hjust = 0.5),
+                        range = c(1, 10)) +
+  facet_wrap(~Espécie) +
+  labs(x = NULL,
+       y = NULL,
+       colour = NULL) +
+  theme_bw() +
+  theme(axis.text = element_text(color = "black", size = 15),
+        legend.text = element_text(color = "black", size = 15),
+        legend.title = element_text(color = "black", size = 15),
+        strip.text = ggtext::element_markdown(color = "black", size = 15),
+        strip.background = element_rect(color = "black", linewidth = 1),
+        legend.position = c(1, 0.08),
+        legend.direction = "horizontal",
+        legend.box = "vertical",
+        legend.justification = c("right", "bottom"),
+        legend.background = element_blank()) +
+  ggview::canvas(height = 14, width = 16)
