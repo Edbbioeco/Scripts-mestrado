143c5e57f0e2abcc7e55b49b01a74d0205ae6000
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-23 18:08:57 -0300

    Criar e exportar mapa

--- mapa_satelite_saltinho.R
+++ mapa_satelite_saltinho.R
@@ -23,3 +23,20 @@ saltinho
 ggplot() +
   tidyterra::geom_spatraster_rgb(data = saltinho) +
   coord_sf(expand = FALSE)
+
+# Mapa ----
+
+ggplot() +
+  tidyterra::geom_spatraster_rgb(data = saltinho) +
+  coord_sf(expand = FALSE,
+           label_graticule = "NSWE") +
+  ggspatial::annotation_scale(localtion = "br",
+                              text_face = "bold",
+                              text_cex = 1.5,
+                              bar_cols = c("black", "gold")) +
+  theme(axis.text = element_text(color = "black", size = 20)) +
+  ggview::canvas(height = 10, width = 12)
+
+ggsave(filename = "mapa_apresentacao_defesa.png",
+       height = 10,
+       width = 12)
