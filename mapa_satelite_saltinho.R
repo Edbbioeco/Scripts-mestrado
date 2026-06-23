92fc359c4bc203274223c96abfeb984dbcad647e
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-23 14:09:23 -0300

    Importar e visualizar o raster de Satinho

--- mapa_satelite_saltinho.R
+++ mapa_satelite_saltinho.R
@@ -9,3 +9,17 @@ library(tidyterra)
 library(ggspatial)
 
 library(ggview)
+
+# Raster de satélite ----
+
+## Importar ----
+
+saltinho <- terra::rast("saltinho.tif")
+
+## Visualizar ----
+
+saltinho
+
+ggplot() +
+  tidyterra::geom_spatraster_rgb(data = saltinho) +
+  coord_sf(expand = FALSE)
