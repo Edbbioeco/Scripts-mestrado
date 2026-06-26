7e1fad17d862d9061ff07c8a71f1a4a5f8096ab9
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-25 21:15:08 -0300

    Cortar a imagem e binariza-la

--- gerar_imagem_dossel.R
+++ gerar_imagem_dossel.R
@@ -16,3 +16,13 @@ img[[4]] |>
 ggsave(filename = "./apresentação/imagem_dossel.png",
        height = 12,
        width = 12)
+
+## Imagem binária ----
+
+img[[4]] |>
+  coiR::coir_crop() |>
+  coiR::coir_binarize()
+
+ggsave(filename = "./apresentação/imagem_dossel_binario.png",
+       height = 12,
+       width = 12)
