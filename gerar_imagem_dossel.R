56c1a47002149b26840e6fd1ba279c23b51643ef
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-25 21:10:14 -0300

    Cortar a imagem

--- gerar_imagem_dossel.R
+++ gerar_imagem_dossel.R
@@ -1,7 +1,18 @@
 # Pacotes ----
 
-library(coirR)
+library(coiR)
 
 # Dados ----
 
 img <- coiR::data_coir()
+
+# Imagens ----
+
+## Imagem normal ----
+
+img[[4]] |>
+  coiR::coir_crop()
+
+ggsave(filename = "./apresentação/imagem_dossel.png",
+       height = 12,
+       width = 12)
