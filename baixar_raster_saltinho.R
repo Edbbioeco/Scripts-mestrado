c1a3c3a740d9abda177df1f67ea09f2b193ea923
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-30 15:22:04 -0300

    Catálogo das cenas

--- baixar_raster_saltinho.R
+++ baixar_raster_saltinho.R
@@ -30,3 +30,17 @@ cliente <- CDSE::GetOAuthClient(id = Sys.getenv("CDSE_ID"),
                                 secret = Sys.getenv("CDSE_SECRET"))
 
 cliente
+
+## Catálogo ----
+
+catalogo <- CDSE::SearchCatalog(aoi = saltinho,
+                                from = "2010-01-01",
+                                to = "2026-05-01",
+                                collection = "sentinel-2-l2a",
+                                with_geometry = FALSE,
+                                client = cliente,
+                                filter = "eo:cloud_cover < 5")
+
+catalogo
+
+catalogo |> dplyr::glimpse()
