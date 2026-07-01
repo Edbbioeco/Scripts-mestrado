0d1750c1b39ad8fb09d5005e9a7223da061fc2c1
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-01 15:00:45 -0300

    Importar e visualizar o shapefile das rodovias do Brasil

--- cortar_saltinho_por_rodovia.R
+++ cortar_saltinho_por_rodovia.R
@@ -18,3 +18,17 @@ borda
 
 ggplot() +
   geom_sf(data = borda, color = "black")
+
+## Rodovias ----
+
+### Importar ----
+
+rodovias <- sf::st_read("C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/SNV_202604A.shp")
+
+### Visualizar ----
+
+rodovias
+
+ggplot() +
+  geom_sf(data = rodovias, color = "black")
+
