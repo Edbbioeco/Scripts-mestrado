5fb9cc17f2a8df1b51eea181e3563f4f7282d0ab
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-01 15:29:23 -0300

    Importar, filtrar para Pernambuco  e visualizar o shapefile de Pernambuco

--- cortar_saltinho_por_rodovia.R
+++ cortar_saltinho_por_rodovia.R
@@ -23,12 +23,31 @@ ggplot() +
 
 ### Importar ----
 
-rodovias <- sf::st_read("C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/SNV_202604A.shp")
+rodovias <- sf::st_read("C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/GEOFT_TRECHO_RODOVIARIO_ESTADUAL.shp")
+
+### Tratar ----
+
+rodovias_pe <- rodovias |>
+  dplyr::filter(UF_SIGLA == "PE")
 
 ### Visualizar ----
 
-rodovias
+rodovias_pe
 
 ggplot() +
-  geom_sf(data = rodovias, color = "black")
+  geom_sf(data = rodovias_pe, color = "black") +
+  geom_sf(data = borda, color = "red", fill = "transparent")
 
+ggplot() +
+  geom_sf(data = rodovias_pe, color = "black") +
+  geom_sf(data = borda, color = "red", fill = "transparent") +
+  coord_sf(xlim = c(-35.20064, -35.15706),
+           ylim = c(-8.744357, -8.712321))
+
+# Recortar a mata de Saltinho ----
+
+## Cortar ----
+
+borda |>
+  lwgeom::st_split(rodovias) |>
+  plot()
