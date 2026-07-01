dc52e3ff26494dd6ff62e1c94f1534a870124999
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-01 15:42:17 -0300

    Criar mapa interativo

--- cortar_saltinho_por_rodovia.R
+++ cortar_saltinho_por_rodovia.R
@@ -21,25 +21,13 @@ ggplot() +
 
 ## Rodovias ----
 
-### Importar ----
+### Criar mapa interativo ----
 
-rodovias <- sf::st_read("C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/GEOFT_TRECHO_RODOVIARIO_ESTADUAL.shp")
-
-### Tratar ----
-
-rodovias_pe <- rodovias |>
-  dplyr::filter(UF_SIGLA == "PE")
-
-### Visualizar ----
-
-rodovias_pe
-
-ggplot() +
-  geom_sf(data = rodovias_pe, color = "black") +
-  geom_sf(data = borda, color = "red", fill = "transparent")
-
-ggplot() +
-  geom_sf(data = rodovias_pe, color = "black") +
-  geom_sf(data = borda, color = "red", fill = "transparent") +
-  coord_sf(xlim = c(-35.20064, -35.15706),
-           ylim = c(-8.744357, -8.712321))
+leaflet::leaflet() |>
+  leaflet::addProviderTiles(providers$Esri.WorldImagery) |>
+  leaflet.extras::addDrawToolbar(
+    editOptions = leaflet.extras::editToolbarOptions()) |>
+  leafem::addMouseCoordinates() |>
+  leaflet::addPolygons(data = borda,
+                       color = "red",
+                       fillOpacity = 0)
