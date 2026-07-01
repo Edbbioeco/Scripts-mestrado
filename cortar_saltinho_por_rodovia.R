8e07048251530668cf56fe615c009eeff2c87711
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-01 14:57:46 -0300

    Importar e visualizar o shapefile da borda de Saltinho

--- cortar_saltinho_por_rodovia.R
+++ cortar_saltinho_por_rodovia.R
@@ -3,3 +3,18 @@
 library(sf)
 
 library(tidyverse)
+
+# Dados ----
+
+## Borda ----
+
+### Importar ----
+
+borda <- sf::st_read("C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/borda_saltinho.shp")
+
+### Visualizar ----
+
+borda
+
+ggplot() +
+  geom_sf(data = borda, color = "black")
