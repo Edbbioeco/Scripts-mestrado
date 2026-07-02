00d663c374c5dbbb08e8b7f42780f38c6bcd47af
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-02 10:55:34 -0300

    Importar e visualizar o raster da imagem de satélite de Saltinho

--- completar_borda_saltinho.R
+++ completar_borda_saltinho.R
@@ -10,15 +10,31 @@ library(leaflet.extras)
 
 library(leafem)
 
-# Shapefile da borda ----
+# Dados ----
 
-## Importar ----
+## Shapefile da borda ----
+
+### Importar ----
 
 borda <- sf::st_read("C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/borda_saltinho.shp")
 
-## Visualizar ----
+### Visualizar ----
 
 borda
 
 ggplot() +
   geom_sf(data = borda, color = "black")
+
+## Raster de Saltinho ----
+
+### Importar ----
+
+saltinho <- terra::rast("C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/saltinhocdse_rgb.tif")
+
+### Visualizar ----
+
+saltinho
+
+ggplot() +
+  tidyterra::geom_spatraster_rgb(data = saltinho) +
+  geom_sf(data = borda, color = "red", fill = "transparent")
