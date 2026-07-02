8fc88463b9ab4959f2bb6dcefafe43f41e6d7fe4
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-02 10:48:12 -0300

    Importar e visualizar o shapefile da borda da mata de Saltinho

--- completar_borda_saltinho.R
+++ completar_borda_saltinho.R
@@ -9,3 +9,16 @@ library(leaflet)
 library(leaflet.extras)
 
 library(leafem)
+
+# Shapefile da borda ----
+
+## Importar ----
+
+borda <- sf::st_read("C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/borda_saltinho.shp")
+
+## Visualizar ----
+
+borda
+
+ggplot() +
+  geom_sf(data = borda, color = "black")
