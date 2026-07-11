5ac9e9ed9dd267eee8171204c65c2dcfb94b2330
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-11 19:58:37 -0300

    Importar imagens

--- cortar_imagens_especies.R
+++ cortar_imagens_especies.R
@@ -7,3 +7,15 @@ library(terra)
 library(tidyterra)
 
 library(coiR)
+
+# Imagens ----
+
+## Importar ----
+
+imagens <- purrr::map(list.files(path = "C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/imagens_especies",
+                      full.names = TRUE),
+                      terra::rast) |>
+  setNames(list.files(path = "C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/dados/imagens_especies") |>
+             stringr::str_remove_all(".jppeg|.JPG"))
+
+imagens
